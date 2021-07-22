5278571c2bbc65c6d30fb461a80b94d5f5ba95de
Abhinav Dixit <abhinavdixit2306@gmail.com>
2021-07-23 01:02:12 +0530

    Update and rename 20july  Lowest Common Ancestor of a Binary Search Tree.cpp to 20julyShuffle an Array.cpp

---
+++ 20julyShuffle an Array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    vector<int> nums;
+public:
+    Solution(vector<int> nums) {
+        this->nums = nums;
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return nums;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        vector<int> res(nums);
+        int n = res.size();
+        for(int i=0; i<n; i++){
+            int it = rand()%(n-i);
+            swap(res[i+it], res[i]);
+        }
+        return res;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * vector<int> param_1 = obj->reset();
+ * vector<int> param_2 = obj->shuffle();
+ */
